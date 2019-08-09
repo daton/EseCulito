5a179828c82759f35ff8a2ecb6ae9c7290d3be08
campitos <rapidclimate@gmail.com>
2019-08-09 00:11:15 -0500

    Hola

---
+++ app/src/main/java/org/unitec/eseculito/AlumnoListAdapter.kt
@@ -0,0 +1,40 @@
+package org.unitec.eseculito
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class AlumnoListAdapter internal constructor(
+        context: Context
+) : RecyclerView.Adapter<AlumnoListAdapter.AlumnoViewHolder>() {
+
+    private val inflater: LayoutInflater = LayoutInflater.from(context)
+    private var alumnos = emptyList<AlumnoEntity>() // Cached copy of words
+
+    inner class AlumnoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val nombreItemView: TextView = itemView.findViewById(R.id.textView)
+
+        //Aqui irian otras vistas invocadas con el view
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlumnoViewHolder {
+        val itemView = inflater.inflate(R.layout.recyclerview_item, parent, false)
+        return AlumnoViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: AlumnoViewHolder, position: Int) {
+        val current = alumnos[position]
+        holder.nombreItemView.text = current.nombre
+    }
+
+    internal fun setWords(words: List<AlumnoEntity>) {
+        this.alumnos = words
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount() = alumnos.size
+}
+
---
+++ app/src/main/java/org/unitec/eseculito/NewAlumnoActivity.kt
@@ -0,0 +1,13 @@
+package org.unitec.eseculito
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NewAlumnoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_alumno)
+    }
+}
